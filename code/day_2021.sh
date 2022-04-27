765994de0c7d2264d34fe16b1e3731ed1bc673b3
oliviaplamann <78232022+oliviaplamann@users.noreply.github.com>
2022-04-27 18:03:43 -0500

    combine days for 2021

---
+++ code/day_2021.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+n=$SLURM_ARRAY_TASK_ID
+
+for d in $(seq 1 31)
+do
+   if [[ ($d -lt 10) ]]
+   then
+	if [[ ($n -lt 10) ]]
+	then
+	awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_0${n}_0$d*.csv > 2021_0${n}_0$d.csv
+
+	else
+	awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_${n}_0$d*.csv > 2021_${n}_0$d.csv
+	fi
+   else
+   	if [[ ($n -lt 10) ]]
+	then
+	awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_0${n}_$d*.csv > 2021_0${n}_$d.csv
+
+	else
+	awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_${n}_$d*.csv > 2021_${n}_$d.csv
+	fi
+   fi
+done   
