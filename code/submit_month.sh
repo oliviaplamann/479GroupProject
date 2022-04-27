a80a7ff1f607c6d1db51e4f07df5ef42916ccc78
sbattula04 <87633591+sbattula04@users.noreply.github.com>
2022-04-27 17:42:56 -0500

    final submit months

---
+++ code/submit_month.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir -p slurm_out_month
+
+jobId1=$(sbatch --array=1-3 \
+       --output="slurm_out_month/slurm-%A_%a.out" \
+       --error="slurm_out_month/slurm-%A_%a.err" \
+       ./month_2022.sh)
+
+jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
+
+jobId2=$(sbatch --dependency=afterok:$jobId1 \
+		--array=1-12 \
+                --output="slurm_out_month/slurm-%A_%a.out" \
+       		--error="slurm_out_month/slurm-%A_%a.err" \
+       		./month_2021.sh)
+
+jobId2=$(echo $jobId2 | sed 's/Submitted batch job //')
+
+jobId3=$(sbatch --dependency=afterok:$jobId2 \
+		--array=1-12 \
+                --output="slurm_out_month/slurm-%A_%a.out" \
+       		--error="slurm_out_month/slurm-%A_%a.err" \
+       		./month_2020.sh)
+
+jobId3=$(echo $jobId3 | sed 's/Submitted batch job //')
