5f6c24ee08060a0e2c8c4129be7d5685a80ad4f8
sbattula04 <87633591+sbattula04@users.noreply.github.com>
2022-04-27 17:40:51 -0500

    2021 months

---
+++ code/month_2021.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+n=$SLURM_ARRAY_TASK_ID
+
+if [[ ($n -lt 10) ]]
+then
+awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_0${n}*.csv > 2021_0${n}.csv
+
+else
+awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2021_${n}*.csv > 2021_${n}.csv
+
+fi
