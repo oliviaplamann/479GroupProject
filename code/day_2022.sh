8fdb661c70d0d189513acbcf9149048b381999e2
oliviaplamann <78232022+oliviaplamann@users.noreply.github.com>
2022-04-27 18:06:24 -0500

    combine days for 2022

---
+++ code/day_2022.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+n=$SLURM_ARRAY_TASK_ID
+
+for d in $(seq 1 31)
+do 
+	if [[ ($d -lt 10) ]]
+	then
+        awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2022_0${n}_0$d*.csv > 2022_0${n}_0$d.csv
+
+	else
+	awk ' { x=1 ; if ( x == NR ) { print "Date, Tweet_ID, Sentiment_Label, Logits_Neutral, Logits_Positive, Logits_Negative" } else { gsub("_Summary_Sentiment.csv","",FILENAME); print FILENAME","$0 } } ' 2020_0${n}_$d*.csv > 2020_0${n}_$d.csv
+	fi
+done
