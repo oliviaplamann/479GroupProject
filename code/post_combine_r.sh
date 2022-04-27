273517b7d44e94353e5e9dd352ea02a7dc07741c
sbattula04 <87633591+sbattula04@users.noreply.github.com>
2022-04-27 17:41:41 -0500

    combine csv

---
+++ code/post_combine_r.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat sentiment_summary*.csv > sentiment_month.csv
+cat sentiment_summary_2020*.csv > sentiment_month_2020.csv
+cat sentiment_summary_2021*.csv > sentiment_month_2021.csv
+cat sentiment_summary_2022*.csv > sentiment_month_2022.csv
