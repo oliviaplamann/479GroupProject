164be527eb855aff4e2fd30dd4ac2ed73bc16fef
sbattula04 <87633591+sbattula04@users.noreply.github.com>
2022-04-27 17:42:39 -0500

    submit file to combine months

---
+++ code/submit_combine_months.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir -p slurm_out_month_r
+
+jobId1=$(sbatch --array=1-3 \
+       --output="slurm_out_month_r/slurm-%A_%a.out" \
+       --error="slurm_out_month_r/slurm-%A_%a.err" \
+       ./combine_months_2022.sh)
+
+jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
+
+jobId2=$(sbatch --dependency=afterok:$jobId1 \
+		--array=1-12 \
+                --output="slurm_out_month_r/slurm-%A_%a.out" \
+       		--error="slurm_out_month_r/slurm-%A_%a.err" \
+       		./combine_months_2021.sh)
+
+jobId2=$(echo $jobId2 | sed 's/Submitted batch job //')
+
+jobId3=$(sbatch --dependency=afterok:$jobId2 \
+		--array=1-12 \
+                --output="slurm_out_month_r/slurm-%A_%a.out" \
+       		--error="slurm_out_month_r/slurm-%A_%a.err" \
+       		./combined_months_2020.sh)
+
+jobId3=$(echo $jobId3 | sed 's/Submitted batch job //')
+
+
+jobId4=$(sbatch --dependency=afterok:$jobId3 \
+                --output="slurm_out_month_r/slurm-%A_%a.out" \
+                --error="slurm_out_month_r/slurm-%A_%a.err" \
+                ./post_combine_r.sh)
+
+jobId4=$(echo $jobId4 | sed 's/Submitted batch job //')
