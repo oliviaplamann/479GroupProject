5ecb066c83b338b01a152873cd26fe3ad5fd15fa
oliviaplamann <78232022+oliviaplamann@users.noreply.github.com>
2022-05-05 11:22:45 -0500

    clean up for tar file

--- code/submit_sentiment_2020.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p slurm_out_sentiment_2020
-
-jobId1=$(sbatch --array=0-23 \
-       --output="slurm_out_sentiment_2020/slurm-%A_%a.out" \
-       --error="slurm_out_sentiment_2020/slurm-%A_%a.err" \
-       ./read_sentiment_2020.sh)
-
-jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
