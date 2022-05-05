acab631b89e32464d40529772e22fc7c7b9f7402
oliviaplamann <78232022+oliviaplamann@users.noreply.github.com>
2022-05-05 11:18:19 -0500

    clean up for tar file

--- code/alldate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-find . -iname "2022*.csv" -print0 | tail -n +2 | xargs -0 awk 'NR == 1 {print $0 ",Date"; next;} FRN == 1 {next;} {print $0 "," FILENAME;}' > 2022.csv
-find . -iname "2021*.csv" -print0 | tail -n +2 | xargs -0 awk 'NR == 1 {print $0 ",Date"; next;} FRN == 1 {next;} {print $0 "," FILENAME;}' > 2021.csv
-find . -iname "2020*.csv" -print0 | tail -n +2 | xargs -0 awk 'NR == 1 {print $0 ",Date"; next;} FRN == 1 {next;} {print $0 "," FILENAME;}' > 2020.csv
