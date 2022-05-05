57f6e1fdd2623a53c5acdd83661ca4e7e87e1ae2
oliviaplamann <78232022+oliviaplamann@users.noreply.github.com>
2022-05-05 11:20:47 -0500

    clean up for tar file

--- code/post_combine_r.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat sentiment_summary*.csv > sentiment_month.csv
-cat sentiment_summary_2020*.csv > sentiment_month_2020.csv
-cat sentiment_summary_2021*.csv > sentiment_month_2021.csv
-cat sentiment_summary_2022*.csv > sentiment_month_2022.csv
